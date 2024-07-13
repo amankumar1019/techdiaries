8ee2cc57a17f89080d53d568f37d7ce0824119ff
Arpita Raikwar <arpitashastri63@gmail.com>
2024-07-13 17:39:18 +0530

    Tut:3 how static and template folder works, how dynamic values are passed to templates

--- tut1.py
+++ tut1.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hemlo World"
+    return render_template('index.html')
 
-@app.route("/harry")
-def harry():
-    return "Hemlo Harry bhai! Bullaa"
+@app.route("/about")
+def about():
+    namee="Arya"
+    return render_template('about.html', name2=namee)
 
 app.run(debug=True)
