32ac1afa800b3e90767f69148c0139eb024271a1
Arpita Raikwar <arpitashastri63@gmail.com>
2024-07-14 19:59:36 +0530

    BL-01 added config file for easy customization and security

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 
-# Replace with your actual MySQL username, password, database name, and socket path
-DB_USERNAME = 'root'
-DB_PASSWORD = ''
-DB_NAME = 'BlogApp'
-DB_SOCKET = '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
+with open('config.json') as json_data:
+    d = json.load(json_data)
+    json_data.close()
 
+localHost = True
+
+# Database parameters initialization for DB connection
+db_params = d["db_params"]["localHost"] if localHost else d["db_params"]["Prod"]
+DB_USERNAME = db_params["DB_USERNAME"]
+DB_PASSWORD = db_params["DB_PASSWORD"]
+DB_NAME = db_params["DB_NAME"]
+DB_SOCKET = db_params["DB_SOCKET"]
+DB_URI = f'mysql+mysqldb://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}?unix_socket={DB_SOCKET}'
+
+social_media_params = d["params"]["social_media"]
 # SQLAlchemy configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}?unix_socket={DB_SOCKET}'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -25,11 +35,11 @@ class Contact(db.Model):
     date = db.Column(db.String(12), nullable=False)
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', social_media_params=social_media_params)
 
 @app.route("/about")
 def about():
-    return (render_template('about.html'))
+    return render_template('about.html', social_media_params=social_media_params)
 
 @app.route("/contact", methods=["GET", "POST"])
 def saveMessage():
@@ -39,12 +49,11 @@ def saveMessage():
     user_message = request.form.get("user_message")
     dt = datetime.now()
 
-    if request.method == "GET":
-        return render_template('contact.html')
-    if user_message != '':
-        p = Contact(name=user_name, email=user_email, mobile=user_mobile, message = user_message, date=dt)
-        db.session.add(p)
-        db.session.commit()
-    return redirect('/')
+    if request.method == "POST":
+        if user_message != '':
+            p = Contact(name=user_name, email=user_email, mobile=user_mobile, message = user_message, date=dt)
+            db.session.add(p)
+            db.session.commit()
+    return render_template('contact.html', social_media_params=social_media_params)
 
 app.run(debug=True)
