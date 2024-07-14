d663ca3bd2291d251bea501c9207a442b644b676
Arpita Raikwar <arpitashastri63@gmail.com>
2024-07-14 14:04:04 +0530

    BL-01 added template inheritance

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
 
 @app.route("/about")
 def about():
-    namee = "Arya"
-    return render_template('about.html', name2=namee)
+    return (render_template('about.html'))
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 app.run(debug=True)
